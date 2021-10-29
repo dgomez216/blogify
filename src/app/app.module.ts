47d32313e2d130b931e9bd2df7a0c69d502aff27
Your Name <you@example.com>
2021-10-29 07:50:16 -0500

    Servies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
+// Librerías de Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
+// Componentes del aplicativo :3
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreatePostComponent } from './create-post/create-post.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Librerías importadas de Angular Material (para instalarlo: ng add @angular/material )
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -12,7 +19,12 @@ import { CreatePostComponent } from './create-post/create-post.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTabsModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent implements OnInit {
-
+  content: string="Hola mundo :3";
+  texto: string="";
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  showText(){
+      this.content=this.texto
+  }
 }
