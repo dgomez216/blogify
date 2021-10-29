7d59f2d4c0073a46819dc79858a8da1532208d6e
Your Name <you@example.com>
2021-10-29 07:49:50 -0500

    Adding Material and Doing Some Events

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import {CreatePostComponent} from './create-post/create-post.component';
+const routes: Routes = [
+  {path:'', component: AppComponent},
+  {path: 'create', component: CreatePostComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
